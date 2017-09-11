aa56dd2fa2cd7383dfb75a187737b958b3ff970e
Niel Sarvari <nielsarvari@gmail.com>
2017-09-11 12:18:08 +0200

    JsonAPI config in app config for AMS

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,7 @@ module RailsApiBase
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    ActiveModel::Serializer.config.adapter = :json
+    ActiveModel::Serializer.config.key_transform = :camel_lower
   end
 end
